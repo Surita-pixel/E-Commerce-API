9afa2787838f38f50f62d955ac986d2d5b7bd1da
Eduardo Surita <suritabtc@gmail.com>
2022-06-03 19:07:12 -0500

    finalized the products API

--- ecommerce/main_app/urls.py
+++ ecommerce/main_app/urls.py
@@ -1,10 +1,13 @@
+from django.urls import include, re_path
 from django.urls import path
 
 from . import views
 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'items', views.ItemApiView)
 urlpatterns = [
-    path('', 
-    views.index,
-    name='index'
-    )
+    #path('', views.index,name='index'),
+    re_path(r'', include(router.urls))
 ]
--- ecommerce/main_app/views.py
+++ ecommerce/main_app/views.py
@@ -1,7 +1,22 @@
+# Django 
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
+# Django Rest Framework
+from rest_framework import viewsets
+
+# Moduls
+from main_app.models import Item
+from main_app.serializers import ItemSerializer
+
+
 def index(request):
+   """this funtion will see if the endpoint is
+      main_app/
+      and just print a hello word
+   """
    return HttpResponse("Hello word")
+
+class ItemApiView(viewsets.ModelViewSet):
+   queryset= Item.objects.all()
+   serializer_class= ItemSerializer
